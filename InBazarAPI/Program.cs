0a8117eb61adf2cd0639c00433865aa273b7e1a1
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-02-21 22:43:40 +0500

    add

--- InBazarAPI/Program.cs
+++ InBazarAPI/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbConTextes(builder.Configuration);
+builder.Services.AddRepository();
 
 var app = builder.Build();
 
