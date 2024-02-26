8f2c406138bfb80e051060084724a4aeb47808cd
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-02-26 20:58:42 +0500

    add

---
+++ InBazar.Service/DTOs/UserDto.cs
@@ -0,0 +1,22 @@
+﻿using InBazar.Domain.Entity.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InBazar.Service.DTOs
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public List<Product> Products { get; set; }
+
+        public string Payment { get; set; }
+    }
+}
