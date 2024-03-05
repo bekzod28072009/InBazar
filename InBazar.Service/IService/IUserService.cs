46d3e2b4ca3237965217bc438ce91d8197895d53
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-03-05 21:19:59 +0500

    add

--- InBazar.Service/IService/IUserService.cs
+++ InBazar.Service/IService/IUserService.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using InBazar.Domain.Entity.Products;
+using InBazar.Domain.Entity.Users;
+using InBazar.Service.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +12,10 @@ namespace InBazar.Service.IService
 {
     public interface IUserService
     {
+        IQueryable<User> GetAll(Expression<Func<User, bool>> expression, string[] includes = null);
+        ValueTask<UserDto> GetAsync(Expression<Func<User, bool>> expression, string[] includes = null);
+        ValueTask<UserDto> CreateAsync(UserDto entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<User, bool>> expression);
+        UserDto Update(int id, UserDto entity);
     }
 }
