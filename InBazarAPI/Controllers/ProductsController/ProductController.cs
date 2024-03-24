ed096880a19d93a5cf7f7ffd4c22c1a360848f08
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2024-03-24 11:04:00 +0500

    add

--- InBazarAPI/Controllers/ProductsController/ProductController.cs
+++ InBazarAPI/Controllers/ProductsController/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InBazar.Service.DTOs;
+using InBazar.Service.IService;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InBazarAPI.Controllers.ProductsController
@@ -7,5 +9,29 @@ namespace InBazarAPI.Controllers.ProductsController
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService service;
+        public ProductController(IProductService service)
+        {
+            this.service = service;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync(ProductDto dto)
+            => Ok(await service.CreateAsync(dto));
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllAsync()
+            => Ok(service.GetAll(p => p.Id != 0));
+
+        [HttpPut("{id}")]
+        public async ValueTask<IActionResult> UpdateAsync([FromRoute] int id, ProductDto dto)
+            => Ok(service?.Update(id, dto));
+
+        [HttpGet("{id}")]
+        public async ValueTask<IActionResult> GetAsync([FromRoute] int id)
+            => Ok(await service.GetAsync(u => u.Id == id));
+
+        [HttpDelete("{id}")]
+        public async ValueTask<IActionResult> DeleteAsync([FromRoute] int id)
+            => Ok(await service.DeleteAsync(p => p.Id == id));
     }
 }
